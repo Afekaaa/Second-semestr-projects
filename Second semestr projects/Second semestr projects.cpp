4456186119052a32f2a4376b229aaffd27560e8f
Zykov Vladimir <flesch.vz@mail.ru>
2023-03-09 21:01:48 +0700

    Реализованы вспомогательные функции для фазы разбиения
    getFirstIdealPartition,
    getFirstMissingSegments,
    createNextMissingSegmentsAndIdealPartition.

--- Second semestr projects/Second semestr projects.cpp
+++ Second semestr projects/Second semestr projects.cpp
@@ -9,14 +9,20 @@ bool createFileWithRandomNumber(const std::string fileName, const int amountNumb
 
 bool isFileContainsSortedArray(const std::string& fileName);
 
-int createFileAndStartSorting(const std::string fileName, const int amountNumbers, const int maxNumbersValue, const int fibonacciNumberOrder = 2);
 
-std::ofstream createSupportsFile(const std::string fileName, const int fibonacciNumberOrder); // рализовать возврат массива вспомогательных файлов
+std::ofstream* createSupportFiles(const std::string fileName, const int fibonacciNumberOrder); // рализовать возврат массива вспомогательных файлов
 
+int* getFirstIdealPartition(const int fibonacciNumberOrder);
+int* getFirstMissingSegments(const int fibonacciNumberOrder);
 
-int* getNextIdealPartition(); //реазлизовать возварт массива идеальных разбиений для слудющей итерации разбиения файла на вспомогательные
+int* getNextIdealPartition(const int * idealPartition, const int fibNumOrder);
+int* getNextMissingSegments(int* missingSegments, const int* idealPartition, const int* newIdealPartition, const int fibNumOrder);
+void createNextMissingSegmentsAndIdealPartition(int* missingSegments, int*& idealPartition, const int fibNumOrder);
 
 void fileSorting(const std::string fileName, const int fibonacciNumberOrder); // реализовать многофазную сортировку
+
+
+int createFileAndStartSorting(const std::string fileName, const int amountNumbers, const int maxNumbersValue, const int fibonacciNumberOrder = 2);
 
 
 int main()
@@ -24,8 +30,8 @@ int main()
     std::string fileName = "fileOfNumbers.txt";
     const int amountNumbers = 100;
     const int maxNumbersValue = 10000;
-    const int amountTests = 10;
-    int fibonacciNumberOrder = 2;
+    const int amountTests = 1;
+    int fibonacciNumberOrder = 5;
 
     for (int i = 1; i < amountTests + 1; ++i)
     {
@@ -48,6 +54,16 @@ int main()
     return 0;
 }
 
+void printMas(int* mas, int len) 
+{
+    for (int i = 0; i < len; ++i) 
+    {
+        std::cout << mas[i] << " ";
+    }
+
+    std::puts("");
+}
+
 int getRandomNumber(int maxNumbersValue) 
 {
     std::random_device rd;
@@ -55,7 +71,6 @@ int getRandomNumber(int maxNumbersValue)
     std::uniform_int_distribution<> dis(0, maxNumbersValue);
     return dis(gen);
 }
-
 bool createFileWithRandomNumber(const std::string fileName, const int amountNumbers, const int maxNumbersValue) 
 {
     srand(time(0));
@@ -108,9 +123,73 @@ int createFileAndStartSorting(const std::string fileName, const int amountNumber
     return 1;
 }
 
-void fileSorting(const std::string fileName, int fibonacciNumberOrder)
+int* getFirstIdealPartition(const int fibonacciNumberOrder) 
 {
-    std::ofstream supFiles = createSupportsFile(fileName, fibonacciNumberOrder); //вернуть массив файлов для многофазной сортировки
+    int* idealPartition = new int[fibonacciNumberOrder];
+
+    for (int i = 0; i < fibonacciNumberOrder; ++i) 
+        idealPartition[i] = 1;
+
+    return idealPartition;
+}
+int* getFirstMissingSegments(const int fibonacciNumberOrder) 
+{
+    int* missingSegments = new int[fibonacciNumberOrder];
+    for (int i = 0; i < fibonacciNumberOrder; ++i)
+        missingSegments[i] = 1;
+
+    return missingSegments;
+}
+
+int* getNextIdealPartition(const int * idealPartition, const int fibNumOrder)
+{
+    int tmpIdealPart0 = idealPartition[0];
+
+    int* newIdealPartition = new int[fibNumOrder];
+
+    for (int i = 0; i < fibNumOrder - 1; ++i)
+        newIdealPartition[i] = tmpIdealPart0 + idealPartition[i + 1];
+
+    newIdealPartition[fibNumOrder - 1] = tmpIdealPart0;
+
+    return newIdealPartition;
+}
+int* getNextMissingSegments(int* missingSegments, const int * idealPartition, const int* newIdealPartition, const int fibNumOrder)
+{
+    for (int i = 0; i < fibNumOrder; ++i) 
+        missingSegments[i] += newIdealPartition[i] - idealPartition[i];  
+
+    return missingSegments;
+}
+
+void createNextMissingSegmentsAndIdealPartition(int* missingSegments, int*& idealPartition, const int fibNumOrder)
+{
+    int* newIdealPartition = getNextIdealPartition(idealPartition, fibNumOrder);
+    missingSegments = getNextMissingSegments(missingSegments, idealPartition, newIdealPartition, fibNumOrder);
+
+    delete idealPartition;
+    idealPartition = newIdealPartition;
+}
+
+std::ofstream* createSupportFiles(const std::string fileName, const int fibonacciNumberOrder) 
+{
+    int* idealPartition = getFirstIdealPartition(fibonacciNumberOrder);
+    int* missingSegments = getFirstMissingSegments(fibonacciNumberOrder);
+
+    std::ofstream* file = new std::ofstream[fibonacciNumberOrder];
+
+
+    createNextMissingSegmentsAndIdealPartition(missingSegments, idealPartition, fibonacciNumberOrder);
+
+    delete[] idealPartition;
+    delete[] missingSegments;
+    return file;
+    
+}
+
+void fileSorting(const std::string fileName, const int fibonacciNumberOrder)
+{
+    std::ofstream* supFiles = createSupportFiles(fileName, fibonacciNumberOrder); //вернуть массив указателей на файлы для многофазной сортировки
     return;
 }
 
