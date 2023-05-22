b7e453eba553ccf8d21a2f53f26c35c2e761e40a
Zykov Vladimir <flesch.vz@mail.ru>
2023-05-22 22:15:18 +0700

    Реализован класс Node
    Класс Node располагается внутри класса HuffmanTree. 
    Реализованы следующие функции:
    1) Node()
    2) Геттеры
    3) Сеттеры
    4) operator +

--- HuffmanTree/HuffmanNode.cpp
+++ HuffmanTree/HuffmanNode.cpp
@@ -1,2 +1,52 @@
-#include "HufmanTree.h"
+#include "HuffmanTree.h"
 
+HuffmanTree::Node::Node(const std::string symbols)
+{
+    for (int i = 0; symbols[i]; i++, m_symbols = symbols);
+    m_symbols += '\0';
+}
+
+int HuffmanTree::Node::getFrequency() const
+{
+    return m_frequency;
+}
+
+std::string HuffmanTree::Node::getSymbols() const
+{
+    return m_symbols;
+}
+
+HuffmanTree::Node* HuffmanTree::Node::getLeftChild()
+{
+    return m_leftChild;
+}
+
+HuffmanTree::Node* HuffmanTree::Node::getRightChild()
+{
+    return m_rightChild;
+}
+
+void HuffmanTree::Node::setFrequency(const int frequency)
+{
+    m_frequency = frequency;
+}
+
+void HuffmanTree::Node::setLeftChild(Node* leftChild)
+{
+    delete m_leftChild;
+    m_leftChild = new Node(leftChild->getSymbols());
+}
+
+void HuffmanTree::Node::setRightChild(Node* rightChild)
+{
+    delete m_rightChild;
+    m_rightChild = new Node(rightChild->getSymbols());
+}
+
+HuffmanTree::Node* HuffmanTree::Node::operator + (Node* other)
+{   
+    Node* newNode = new Node(m_symbols + other->getSymbols());
+    newNode->setFrequency(m_frequency + other->getFrequency());
+    newNode->setLeftChild(this);
+    newNode->setRightChild(other);
+}
--- HuffmanTree/HuffmanTree.h
+++ HuffmanTree/HuffmanTree.h
@@ -1,11 +1,42 @@
 #pragma once
+
+#include <iostream>
+
 class HuffmanTree
 {
-	class HuffmanNode 
+public:
+	class Node 
 	{
 	public: 
+		Node() = default;
+		Node(const std::string symbols);
+		~Node() = default;
+
+		int getFrequency() const;
+		std::string getSymbols() const;
+		Node* getLeftChild();
+		Node* getRightChild();
+
+		void setFrequency(const int frequency);
+		void setLeftChild(Node* leftChild);
+		void setRightChild(Node* rightCild);
+
+		Node* operator + (Node* otherNode);
+
+
 	private:
+		std::string m_symbols;
+		int m_frequency = 1;
+		Node* m_leftChild = nullptr;
+		Node* m_rightChild = nullptr;
 
 	};
+
+	std::string build(std::string textBeforEncryption);
+	int encode(std::string textBeforEncryption, std::string* textAfterEncryption) const;
+	bool decode(std::string textAfterEncryption, std::string* textBeforEncryption);
+
+private:
+	Node* m_root = nullptr;
 };
 
