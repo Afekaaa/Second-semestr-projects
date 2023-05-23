af9b982fcdfd10f63d851ed2252e238de3726c20
Zykov Vladimir <flesch.vz@mail.ru>
2023-05-23 11:37:08 +0700

    Добавлен конструктор копирования и оператор ++

--- HuffmanTree/HuffmanNode.cpp
+++ HuffmanTree/HuffmanNode.cpp
@@ -1,9 +1,20 @@
 #include "HuffmanTree.h"
 
+HuffmanTree::Node::Node(const Node& other)
+{
+    m_symbols = other.getSymbols();
+    m_frequency = other.getFrequency();
+}
+
 HuffmanTree::Node::Node(const std::string symbols)
 {
     for (int i = 0; symbols[i]; i++, m_symbols = symbols);
     m_symbols += '\0';
+}
+
+HuffmanTree::Node::Node(const char symbol)
+{
+    m_symbols = symbol + '\0';
 }
 
 int HuffmanTree::Node::getFrequency() const
@@ -16,12 +27,12 @@ std::string HuffmanTree::Node::getSymbols() const
     return m_symbols;
 }
 
-HuffmanTree::Node* HuffmanTree::Node::getLeftChild()
+HuffmanTree::Node* HuffmanTree::Node::getLeftChild() const 
 {
     return m_leftChild;
 }
 
-HuffmanTree::Node* HuffmanTree::Node::getRightChild()
+HuffmanTree::Node* HuffmanTree::Node::getRightChild()const 
 {
     return m_rightChild;
 }
@@ -31,22 +42,29 @@ void HuffmanTree::Node::setFrequency(const int frequency)
     m_frequency = frequency;
 }
 
-void HuffmanTree::Node::setLeftChild(Node* leftChild)
+void HuffmanTree::Node::setLeftChild(const Node* leftChild)
 {
     delete m_leftChild;
     m_leftChild = new Node(leftChild->getSymbols());
 }
 
-void HuffmanTree::Node::setRightChild(Node* rightChild)
+void HuffmanTree::Node::setRightChild(const Node* rightChild)
 {
     delete m_rightChild;
     m_rightChild = new Node(rightChild->getSymbols());
 }
 
-HuffmanTree::Node* HuffmanTree::Node::operator + (Node* other)
+HuffmanTree::Node* HuffmanTree::Node::operator + (Node other)
 {   
-    Node* newNode = new Node(m_symbols + other->getSymbols());
-    newNode->setFrequency(m_frequency + other->getFrequency());
+    Node* newNode = new Node(m_symbols + other.getSymbols());
     newNode->setLeftChild(this);
-    newNode->setRightChild(other);
+    newNode->setRightChild(&other);
+
+    return newNode;
+}
+
+HuffmanTree::Node* HuffmanTree::Node::operator ++ ()
+{
+    m_frequency++;
+    return this;
 }
--- HuffmanTree/HuffmanTree.h
+++ HuffmanTree/HuffmanTree.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <iostream>
+#include <list>
 
 class HuffmanTree
 {
@@ -9,27 +10,28 @@ public:
 	{
 	public: 
 		Node() = default;
+		Node(const char symbol);
 		Node(const std::string symbols);
+		Node(const Node& other);
 		~Node() = default;
 
 		int getFrequency() const;
 		std::string getSymbols() const;
-		Node* getLeftChild();
-		Node* getRightChild();
+		Node* getLeftChild() const;
+		Node* getRightChild() const;
 
 		void setFrequency(const int frequency);
 		void setLeftChild(Node* leftChild);
 		void setRightChild(Node* rightCild);
 
 		Node* operator + (Node* otherNode);
-
+		Node* operator ++ ();
 
 	private:
 		std::string m_symbols;
 		int m_frequency = 1;
 		Node* m_leftChild = nullptr;
 		Node* m_rightChild = nullptr;
-
 	};
 
 	std::string build(std::string textBeforEncryption);
@@ -38,5 +40,9 @@ public:
 
 private:
 	Node* m_root = nullptr;
+
+	void createNodes(std::list<Node*>& nodes, std::string textBeforEncryption);
+	Node* getJoinedTwoNodes(Node* firstNode, Node* secondNode);
+	void setNewNodeToList(std::list<Node*>& nodes, Node* newNode);
 };
 
